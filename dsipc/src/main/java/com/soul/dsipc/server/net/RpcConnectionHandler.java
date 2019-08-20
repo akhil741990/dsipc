fabe7ee1fe1c7616c5f7ee2cc3585dc4953016e7
Akhil Vishwanathan Pillai <akhil741990@gmail.com>
2019-08-20 07:34:02 +0530

    Added RPC packet processing

--- dsipc/src/main/java/com/soul/dsipc/server/net/RpcConnectionHandler.java
+++ dsipc/src/main/java/com/soul/dsipc/server/net/RpcConnectionHandler.java
@@ -1,5 +1,6 @@
 package com.soul.dsipc.server.net;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
@@ -10,15 +11,56 @@ import java.nio.channels.SocketChannel;
 public class RpcConnectionHandler {
 
 	private SocketChannel channel;
-	private ByteBuffer data;
-	private ByteBuffer header;
-	
+	private ByteBuffer dataBuffer;
+	private ByteBuffer headerBuffer;
+	private ByteBuffer dataLengthBuffer;
+	private int dataLength;
 	public RpcConnectionHandler(SocketChannel channel) {
 		this.channel = channel;
+		headerBuffer = ByteBuffer.allocate(3);
+		dataLengthBuffer = ByteBuffer.allocate(4);
 	}
 	
 	
-	public void readRpcRequest(){
+	/*
+	 * 
+		DataLength :
+		 First 4 bytes  
 		
+		Header
+		Next 3 bytes are header :
+				1st byte = version
+				2nd byte = Service class
+				3rd byte = Authentication params
+		Data : 
+			Next dataLength Bytes
+
+	 */
+	public void readRpcRequest(){
+	
+		
+		try {
+			//Reading data length
+			int count = this.channel.read(dataLengthBuffer);
+			
+			if(count < 0){
+				return;
+			}
+			
+			//Reading Header
+			count = this.channel.read(headerBuffer);
+			if(count < 0){
+				return;
+			}
+			
+			//Reading data
+			dataLength = dataLengthBuffer.getInt();
+			this.dataBuffer = ByteBuffer.allocate(dataLength);
+			count = this.channel.read(dataBuffer);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
 }
