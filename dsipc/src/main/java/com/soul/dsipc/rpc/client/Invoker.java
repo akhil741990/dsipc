d72fb4b5ef977a9694da2ffe0b1104411188caee
Akhil Vishwanathan Pillai <akhil741990@gmail.com>
2019-10-22 22:57:56 +0530

    Added .proto file for RpcFRequest

--- dsipc/src/main/java/com/soul/dsipc/rpc/client/Invoker.java
+++ dsipc/src/main/java/com/soul/dsipc/rpc/client/Invoker.java
@@ -8,10 +8,20 @@ public class Invoker implements InvocationHandler{
 
 	InetSocketAddress address;
 	RpcClient client;
-
+    String protocolName;
+	public Invoker(Class<?> protocol){
+		this.protocolName = protocol.getSimpleName();
+	}
+	
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		
 		// Build RpcRequest
+		
+		/*Add proto file for RpcRequestHeader
+		 Header will be constructed from method
+		 Body will be constructed form args[1]
+		*/ 
+		
 		this.client.call();
 		return null;
 	}
--- dsipc/src/main/java/com/soul/dsipc/rpc/client/ProtocolProxy.java
+++ dsipc/src/main/java/com/soul/dsipc/rpc/client/ProtocolProxy.java
@@ -1,5 +1,10 @@
 package com.soul.dsipc.rpc.client;
 
-public class ProtocolProxy {
-
+public class ProtocolProxy<T> {
+	private Class<T> protocol;
+	T proxy;
+	public ProtocolProxy(Class<T> protocol, T proxy){
+		this.protocol = protocol;
+		this.proxy = proxy;
+	}
 }
