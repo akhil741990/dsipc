a359084a1685443820109935a6a87626dcca0113
Akhil Pillai <akhil741990@gmail.com>
2019-11-22 14:33:06 +0530

    Resolved merge conflicts in Invoker

--- dsipc/src/main/java/com/soul/dsipc/rpc/Invoker.java
+++ dsipc/src/main/java/com/soul/dsipc/rpc/Invoker.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 
 import com.soul.dsipc.rpc.client.RpcClient;
+import com.soul.hadoop.common.protobuf.ProtobufRpcEngineProtos.RequestHeaderProto;
 import com.soul.hadoop.common.protobuf.RpcHeaderProtos.RpcRequestHeaderProto;
 
 public class Invoker implements InvocationHandler{
@@ -25,16 +26,20 @@ public class Invoker implements InvocationHandler{
 		 Body will be constructed form args[1]
 		*/ 
 		
+		
+		
+		
 		this.client.call();
 		return null;
 	}
 	
 	
-	 private RpcRequestHeaderProto constructRpcRequestHeader(Method method,long clientProtocolVersion) {
-		 RpcRequestHeaderProto.Builder builder = RpcRequestHeaderProto.newBuilder();
-	      builder.setMethodName(method.getName());
-	      builder.setDeclaringClassProtocolName(protocolName);
-	      builder.setClientProtocolVersion(clientProtocolVersion);
-	      return builder.build();
+	 private RequestHeaderProto constructRpcRequestHeader(Method method,long clientProtocolVersion) {
+		 RequestHeaderProto.Builder builder = RequestHeaderProto
+		          .newBuilder();
+		      builder.setMethodName(method.getName());
+		      builder.setDeclaringClassProtocolName(protocolName);
+		      builder.setClientProtocolVersion(clientProtocolVersion);
+		      return builder.build();
 	    }
 }
