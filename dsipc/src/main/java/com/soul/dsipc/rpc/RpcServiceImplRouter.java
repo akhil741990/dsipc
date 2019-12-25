438e6837a502ec7602aa1d32997b182c52bbac20
Akhil Vishwanathan Pillai <akhil741990@gmail.com>
2019-12-25 12:53:54 +0530

    Added Thread Pool in RpcSvcRouter

--- dsipc/src/main/java/com/soul/dsipc/rpc/RpcServiceImplRouter.java
+++ dsipc/src/main/java/com/soul/dsipc/rpc/RpcServiceImplRouter.java
@@ -3,8 +3,14 @@ package com.soul.dsipc.rpc;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.protobuf.BlockingService;
 import com.google.protobuf.Descriptors.MethodDescriptor;
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -22,6 +28,7 @@ import com.soul.hadoop.common.protobuf.ProtobufRpcEngineProtos.RequestHeaderProt
 public class RpcServiceImplRouter  extends Thread implements EverRunningComponent{
 
 	Map<String,Object> implMap;
+	private ExecutorService rpcSvcRouterExecutor;
 	BlockingQueue<RpcCall> queue = new LinkedBlockingQueue<RpcCall>(64); //TODO : make size configurable
 	
 	public BlockingQueue<RpcCall> getQueue() {
@@ -30,6 +37,11 @@ public class RpcServiceImplRouter  extends Thread implements EverRunningComponen
 
 	public RpcServiceImplRouter(){
 		this.implMap = new HashMap<String, Object>();
+		rpcSvcRouterExecutor =  Executors.newCachedThreadPool(
+	            new ThreadFactoryBuilder()
+	            .setDaemon(true)
+	            .setNameFormat("RpcServiceRouter Executor Thread #%d")
+	            .build());
 		//this.isRunning = true;
 	}
 	
@@ -46,22 +58,35 @@ public class RpcServiceImplRouter  extends Thread implements EverRunningComponen
 				RequestHeaderProto header = call.getReq().getMessageHeader();
 				String methodName = header.getMethodName();
 				String protoName = header.getDeclaringClassProtocolName();
-				BlockingService svc = (BlockingService)implMap.get(protoName);
-				MethodDescriptor methodDescriptor = svc.getDescriptorForType().findMethodByName(methodName);
+				final BlockingService svc = (BlockingService)implMap.get(protoName);
+				final MethodDescriptor methodDescriptor = svc.getDescriptorForType().findMethodByName(methodName);
 				
 				Message prototype = svc.getRequestPrototype(methodDescriptor);
 		        try {
-					Message param = prototype.newBuilderForType()
+					final Message param = prototype.newBuilderForType()
 					    .mergeFrom(call.getReq().getMessageBytes()).build();
-					Message result = svc.callBlockingMethod(methodDescriptor, null, param);
+					
+					
+					Future<Message> resp = rpcSvcRouterExecutor.submit(new Callable<Message>() {
+
+						public Message call() throws Exception {
+							Message result = svc.callBlockingMethod(methodDescriptor, null, param);
+							return result;
+						}
+					});
+					
+					// Add the following blocking call in a New Thread that dispatches the response 
+					try {
+						Message out = resp.get();
+					} catch (ExecutionException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					
 				} catch (InvalidProtocolBufferException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} catch (ServiceException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				} 
 				
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
